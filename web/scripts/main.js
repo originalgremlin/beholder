e9286fd764486921f6e353c3d2446cf02a5d5588
Barry Shapira <barry.shapira@gmail.com>
2013-03-06 16:27:59 +0300

    Got basic timed snapshots working.

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -17,37 +17,46 @@
     define(function (require) {
         var $ = require('jquery');
 
-        var video = $('video'),
-            color = $('img.color'),
-            colorCanvas = $('<canvas height="480" width="640" />')[0],
-            colorContext = colorCanvas.getContext('2d'),
-            gray = $('img.gray'),
-            grayCanvas = $('<canvas height="160" width="120" />')[0],
-            grayContext = grayCanvas.getContext('2d');
+        // TODO: add dots for eyes and nose, or a square for the head
+        // TODO: pull out a rectangle which contains the face but little else
+        // TODO: turn that rectangle into grayscale
+        // TODO: send a subset of the grayscale data to the server for saving
 
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
         navigator.getUserMedia({ video: true }, function (stream) {
+            var video = $('video'),
+                ul = $('ul'),
+                canvas = $('<canvas height="480" width="640" />')[0],
+                context = canvas.getContext('2d');
+
             video.attr({ src: window.URL.createObjectURL(stream) });
+            drawHeadRectangle();
 
-            video.on('click', function (event) {
-                colorContext.drawImage(video[0], 0, 0, 640, 480);
-                // color
-                color.attr({ src: colorCanvas.toDataURL('image/jpeg') });
-                // grayscale
-                var imageData = colorContext.getImageData(0, 0, 160, 120),
-                    data = imageData.data;
-                for (var i = 0, length = data.length; i < length; i += 4) {
-                    var luminance = Math.floor((data[i] + data[i+1] + data[i+2]) / 3);
-                    data[i] = data[i+1] = data[i+2] = luminance;
-                }
-                grayContext.putImageData(imageData, 0, 0);
-                gray.attr({ src: grayCanvas.toDataURL('image/jpeg') });
-            });
-
+            var count = 0, maxCount = 100, delay = 300;
+            var interval = window.setInterval(function () {
+                if (++count >= maxCount)
+                    window.clearInterval(interval);
+                var img, imageData, data;
+                // get image context
+                context.drawImage(video[0], 0, 0, 640, 480);
+                imageData = context.getImageData(0, 0, 640, 480),
+                data = imageData.data;
+                // convert to grayscale
+                for (var i = 0, length = data.length; i < length; i += 3)
+                    data[i] = data[i+1] = data[i+2] = Math.floor(data[i] * 0.30 + data[i+1] * 0.59 + data[i+2] * 0.11);
+                // draw grayscale image
+                context.putImageData(imageData, 0, 0);
+                ul.append($('<li />').append($('<img />').attr({ src: canvas.toDataURL('image/jpeg'), width: 160, height: 120 })));
+            }, delay);
         });
 
-
+        var drawHeadRectangle = function () {
+            var context = $('canvas')[0].getContext('2d');
+            context.clearRect(0, 0, 640, 480);
+            context.setStrokeColor('#ff0000');
+            context.setLineWidth(5);
+            context.strokeRect(170, 40, 300, 400);
+        };
 
         // fire it up
         $(document).ready(function (event) {
