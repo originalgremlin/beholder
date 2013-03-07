f2e8480342af882556bea9999f9ead59ceac0d36
Barry Shapira <barry.shapira@gmail.com>
2013-03-07 21:48:36 +0300

    Added basic audio recording.

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -4,7 +4,11 @@
     require.config({
         paths: {
             jquery: 'lib/jquery',
-            underscore: 'lib/underscore.custom'
+            recorder: 'lib/recorder',
+            underscore: 'lib/underscore'
+        },
+        shim: {
+            'recorder': { exports: 'Recorder' }
         },
         urlArgs: debug ? (new Date()).getTime() : undefined,
         waitSeconds: 60
@@ -15,47 +19,71 @@
     };
 
     define(function (require) {
-        var $ = require('jquery');
+        var $ = require('jquery'),
+            Recorder = require('recorder');
 
-        // TODO: add dots for eyes and nose, or a square for the head
-        // TODO: pull out a rectangle which contains the face but little else
-        // TODO: turn that rectangle into grayscale
-        // TODO: send a subset of the grayscale data to the server for saving
+        window.URL = window.URL || window.webkitURL;
+        window.audioContext = window.audioContext || window.webkitAudioContext;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-        navigator.getUserMedia({ video: true }, function (stream) {
+        navigator.getUserMedia({ audio: true, video: true }, function (stream) {
+            // audio
+            // http://codeartists.com/post/36746402258/how-to-record-audio-in-chrome-with-native-html5-apis
+            // WARNING: need to enable "Web Audio Input" under "chrome://flags/"
+            var acontext = new window.audioContext(),
+                mediaStreamSource = acontext.createMediaStreamSource(stream),
+                recorder = new Recorder(mediaStreamSource);
+
+            // video
             var video = $('video'),
                 ul = $('ul'),
                 canvas = $('<canvas height="480" width="640" />')[0],
                 context = canvas.getContext('2d');
-
             video.attr({ src: window.URL.createObjectURL(stream) });
             drawHeadRectangle();
 
-            var count = 0, maxCount = 100, delay = 300;
-            var interval = window.setInterval(function () {
-                if (++count >= maxCount)
-                    window.clearInterval(interval);
-                var img, imageData, data;
-                // get image context
-                context.drawImage(video[0], 0, 0, 640, 480);
-                imageData = context.getImageData(0, 0, 640, 480),
-                data = imageData.data;
-                // convert to grayscale
-                for (var i = 0, length = data.length; i < length; i += 3)
-                    data[i] = data[i+1] = data[i+2] = Math.floor(data[i] * 0.30 + data[i+1] * 0.59 + data[i+2] * 0.11);
-                // draw grayscale image
-                context.putImageData(imageData, 0, 0);
-                ul.append($('<li />').append($('<img />').attr({ src: canvas.toDataURL('image/jpeg'), width: 160, height: 120 })));
-            }, delay);
+            $('a.startRecording').on('click', function (event) {
+                // record audio for 30 seconds
+                recorder.record();
+                window.setTimeout(function () {
+                    recorder.stop();
+                    recorder.exportWAV(function (s) {
+                        $('audio')[0].src = window.URL.createObjectURL(s);
+                    });
+                }, 5000);
+
+                // take snapshots
+                var count = 0,
+                    maxCount = 10,
+                    delay = 300;
+                var interval = window.setInterval(function () {
+                    if (++count >= maxCount)
+                        window.clearInterval(interval);
+                    var img, imageData, data;
+                    // get image context
+                    context.drawImage(video[0], 0, 0, 640, 480);
+                    imageData = context.getImageData(0, 0, 640, 480),
+                    data = imageData.data;
+                    // convert to grayscale
+                    for (var i = 0, length = data.length; i < length; i += 3) {
+                        data[i] = data[i+1] = data[i+2] = Math.floor(data[i] * 0.30 + data[i+1] * 0.59 + data[i+2] * 0.11);
+                    }
+                    // draw grayscale image
+                    context.putImageData(imageData, 0, 0);
+                    ul.append($('<li />').append($('<img />').attr({ src: canvas.toDataURL('image/jpeg'), width: 160, height: 120 })));
+                }, delay);
+            });
         });
 
         var drawHeadRectangle = function () {
             var context = $('canvas')[0].getContext('2d');
             context.clearRect(0, 0, 640, 480);
+            context.setFillColor("rgba(255, 255, 255, 0.6)");
+            context.fillRect(0, 0, 640, 480);
+            context.clearRect(220, 90, 200, 300);
             context.setStrokeColor('#ff0000');
             context.setLineWidth(5);
-            context.strokeRect(170, 40, 300, 400);
+            context.strokeRect(220, 90, 200, 300);
         };
 
         // fire it up
